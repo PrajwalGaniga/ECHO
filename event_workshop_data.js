8504b872ec7a54723d3f268202745a1c2b1e710c
PrajwalGaniga <prajwalganiga06@gmail.com>
2025-09-14 10:50:22 +0530

    Updated members_data.js and fixed image paths

--- event_workshop_data.js
+++ event_workshop_data.js
@@ -1,9 +1,8 @@
 const eventsData = [
-    {id: 4, title: 'Smart India Hackathon 2025', description: 'A national-level event focused on solving real-world problems using technology. Open to all students.', thumbnail: '/assets/images/events/sih.jpg', college: 'SIT Collage', date: '2025-11-22 & 2025-11-23', time: '10:00 AM', fee: 'Free', status: 'Active'}
+    {id: 4, title: 'Smart India Hackathon 2025', description: 'A national-level event focused on solving real-world problems using technology. Open to all students.', thumbnail: 'assets/images/events/sih.jpg', college: 'SIT Collage', date: '2025-11-22 & 2025-11-23', time: '10:00 AM', fee: 'Free', status: 'Active'}
     
 ];
 
 const workshopsData = [
-    
     {id: 3, title: 'Introduction to AI & Machine Learning', description: 'A beginner-friendly workshop covering the fundamentals of AI, ML, and how to build your first predictive model.', thumbnail: 'assets/images/workshops/workshop3.jpg', college: 'ECHO University - Lab 3', date: '2025-10-10', time: '1:00 PM', fee: 'Free', status: 'Active'}
 ];
