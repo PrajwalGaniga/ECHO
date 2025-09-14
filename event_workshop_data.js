1ddabaf3cc0d00f5013ee354f496795f0a248a4e
PrajwalGaniga <prajwalganiga06@gmail.com>
2025-09-14 23:54:33 +0530

    Profile

--- event_workshop_data.js
+++ event_workshop_data.js
@@ -1,8 +1,8 @@
 const eventsData = [
-    {id: 4, title: 'Smart India Hackathon 2025', description: 'A national-level event focused on solving real-world problems using technology. Open to all students.', thumbnail: 'assets/images/events/event/sih.jpg', college: 'SIT Collage', date: '2025-11-22 & 2025-11-23', time: '10:00 AM', fee: 'Free', status: 'Active'}
+    {id: 4, title: 'Smart India Hackathon 2025', description: 'A national-level event focused on solving real-world problems using technology. Open to all students.', thumbnail: 'assets/images/events/event/sih.jpg', college: 'SIT Collage', date: '2025-11-22', time: '10:00 AM', fee: 'Free', status: 'Active'}
     
 ];
 
 const workshopsData = [
-    {id: 3, title: 'Introduction to AI & Machine Learning', description: 'A beginner-friendly workshop covering the fundamentals of AI, ML, and how to build your first predictive model.', thumbnail: 'assets/images/events/workshop/dhaarini.jpg', college: 'ECHO University - Lab 3', date: '2025-10-10', time: '1:00 PM', fee: 'Free', status: 'Active'}
+    {id: 3, title: 'Introduction to AI & Machine Learning', description: 'A beginner-friendly workshop covering the fundamentals of AI, ML, and how to build your first predictive model.', thumbnail: 'assets/images/events/workshop/dhaarini.jpg', college: 'ECHO University - Lab 3', date: '2025-10-10', time: '1:00 PM', fee: '2500', status: 'Active'}
 ];
